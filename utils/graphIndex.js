d60d4a06c9534af328c80bdd6b80136bf3f8e419
KYO <nsmaomao@users.noreply.github.com>
2019-05-25 20:04:07 +0800

    生成图索引

--- utils/graphIndex.js
+++ utils/graphIndex.js
@@ -4,6 +4,37 @@
 var _ = require('lodash');
 
 var createGraphIndex = function(data) {
+  data.linkIndex = {}
+  data.links = _.map(data.links, n=>{
+    n.id = n.source+''+n.target
+    return n
+  })
+  _.forEach(data.links, (n, k)=>{
+    data.linkIndex[n.id] = k
+  })
+  data.nodes = _.map(data.nodes, n=>{
+    n.sourceLinks = _.filter(data.links, link=>{
+      return link.source==n.id
+    })
+    n.sourceLinks = _.map(n.sourceLinks, n=>{
+      return n.id
+    })
+    n.targetLinks = _.filter(data.links, link=>{
+      return link.target==n.id
+    })
+    n.targetLinks = _.map(n.targetLinks, n=>{
+      return n.id
+    })
+    return n
+  })
+  data.nodeIndex = {}
+  _.forEach(data.nodes, (n, k)=>{
+    data.nodeIndex[n.id] = k
+  })
+  const start = _.find(data.nodes, n=>{
+    return n.type=='start'
+  })
+  data.start = start.id
   return data
 };
 
