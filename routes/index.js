f84e3cace521a38d511b901dd83f62cfa856d353
KYO <nsmaomao@users.noreply.github.com>
2019-05-25 14:35:42 +0800

    界面展示输入输出和图

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var json = require('./json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var input = req.query
+  var output = []
+  res.render('index', {
+    title: 'GL-Server',
+    graph: JSON.stringify(json, null, 2),
+    input: JSON.stringify(input, null, 2),
+    output: JSON.stringify(output, null, 2)
+  });
 });
 
 module.exports = router;
---
+++ routes/json.js
@@ -0,0 +1,12 @@
+var _ = require('lodash');
+
+var json = {
+  nodes: [
+    {
+      id: _.random(1,999999)
+    }
+  ],
+  links: [],
+};
+
+module.exports = json;
