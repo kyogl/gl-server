5a5319c26c16969d91b46e8c73a8bfa859e37604
kyogl <nsmaomao5@gmail.com>
2019-09-28 18:44:27 +0800

    init

--- runtime/op/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  math: require('./math'),
-  condition: require('./condition'),
-  array: require('./array'),
-  string: require('./string')
-}
--- utils/getGraph.js
+++
@@ -1,9 +0,0 @@
-/*
-  获取图数据
-*/
-
-const createGraphIndex = require('./graphIndex')
-
-module.exports = function(graphData) {
-  return createGraphIndex(graphData)
-};
